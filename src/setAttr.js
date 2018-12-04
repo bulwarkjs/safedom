8da50aa5427d3ece8a49cc35180178b8406ea2dd
victufell <victor.fellype.a@gmail.com>
2018-12-04 16:13:07 -0200

    fix: setAttr

--- src/setAttr.js
+++ src/setAttr.js
@@ -2,11 +2,11 @@ const { Ok, Error } = require('folktale/result')
 const { isElementNode } = require('./utils')
 const curry = require('folktale/core/lambda/curry')
 
-const setAttr = curry(2, (attr, node) => {
+const setAttr = curry(3, (attr, value, node) => {
   if (isElementNode(node)) {
-    const value = node.setAttribute(attr)
-    return value
-      ? Ok({ node, value })
+    const element = node.setAttribute(attr, value)
+    return element
+      ? Ok({ node, element })
       : Error(`Attribute: '${attr}' don't found \nNode: ${node}\nMethod: setAttr`)
   }
 
